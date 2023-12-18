375705901c4664c5361bd9182730ee31b8ff0b59
Ekaterina Spitsyna <ekaterina.spitsyna@webpros.com>
2023-12-18 21:02:02 +0100

    Day 16

---
+++ 16/1.js
@@ -0,0 +1,83 @@
+const fs = require('node:fs');
+
+const RIGHT = 1;
+const LEFT = -1;
+const UP = 2;
+const DOWN = -2;
+
+const data = fs.readFileSync('data.txt', 'utf8').split("\r\n").map(row => row.split(''));
+const numRows = data.length;
+const numColumns = data[0].length;
+
+const newMap = [...data.map(line => line.map(_ => []))]
+
+const move = ([i, j], direction) => {
+    let moveI = i;
+    if (direction === DOWN) {
+        moveI++;
+    } else if (direction === UP) {
+        moveI --;
+    }
+    let moveJ = j;
+    if (direction === RIGHT) {
+        moveJ++;
+    } else if (direction === LEFT) {
+        moveJ --;
+    }
+
+    return [moveI, moveJ];
+};
+
+const reflect = direction => {
+    switch (direction) {
+        case RIGHT:
+            return UP;
+        case LEFT:
+            return DOWN;
+        case UP:
+            return RIGHT;
+        case DOWN:
+            return LEFT;
+    }
+};
+
+const exit = ([i, j], direction, map) => i < 0 || i >= numRows || j < 0 || j >= numColumns || map[i][j].includes(direction);
+
+const go = ([i, j], direction, map) => {
+    if (exit([i, j], direction, map)) {
+        return [map];
+    }
+
+    map[i][j].push(direction);
+    switch (data[i][j]) {
+        case '.':
+            return go(move([i, j], direction), direction, map);
+        case '/':
+            const targetDirection1 = reflect(direction);
+            return go(move([i, j], targetDirection1), targetDirection1, map);
+        case '\\':
+            const targetDirection2 = -reflect(direction);
+            return go(move([i, j], targetDirection2), targetDirection2, map);
+        case '|':
+            if (direction === UP || direction === DOWN) {
+                return go(move([i, j], direction), direction, map);
+            }
+            const t1 = move([i, j], UP);
+            const t2 = move([i, j], DOWN);
+            return [map, ...go(t1, UP, newMap), ...go(t2, DOWN, newMap)];
+        case '-':
+            if (direction === LEFT || direction === RIGHT) {
+                return go(move([i, j], direction), direction, map);
+            }
+            const t3 = move([i, j], RIGHT);
+            const t4 = move([i, j], LEFT);
+            return [map, ...go(t3, RIGHT, newMap), ...go(t4, LEFT, newMap)];
+        default:
+            return [map];
+
+    }
+}
+
+const result = go([0, 0], RIGHT, newMap);
+const sum = data.reduce((acc, line, i) => acc + line.reduce((acc, _, j) => acc + (result.some(r => r[i][j].length) ? 1 : 0), 0), 0);
+console.log(sum);
