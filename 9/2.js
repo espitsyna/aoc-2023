214becfeaa82a25298fcf4979498a51f63b46a0f
Ekaterina Spitsyna <ekaterina.spitsyna@webpros.com>
2023-12-18 20:58:33 +0100

    Day 9

---
+++ 9/2.js
@@ -0,0 +1,33 @@
+const fs = require('node:fs');
+
+const data = fs.readFileSync('data.txt', 'utf8').split("\r\n");
+
+const trim = line => {
+    const num = [];
+    let i = 0;
+    while (i < line.length) {
+        let n = '';
+        while (line[i] && ' ' !== line[i]) {
+            n += line[i];
+            i++;
+        }
+        if (n !== '') {
+            num.push(+n);
+        }
+        i++;
+    }
+
+    return num;
+};
+
+const sum = data.map(trim).reduce((acc, line) => {
+    const numbers = [];
+    numbers.push(line);
+    while (!numbers.at(-1).every(n => n === 0)) {
+        const last = numbers.at(-1);
+        numbers.push([...Array(last.length - 1)].map((_, i) => last[i + 1] - last[i]));
+    }
+    return acc + numbers.reverse().reduce((acc, n) => n[0] - acc, 0);
+}, 0);
+
+console.log(sum);
